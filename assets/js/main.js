a796aa652b7814735edbeaebecf173b65982bff3
Pranay <pranayguptamain@gmail.com>
2021-12-21 01:08:43 +0530

    feat: Add broken qualification section

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,6 +46,24 @@ skillsHeader.forEach((e) => {
 });
 
 //  QUALIFICATION TABS
+const tabs = document.querySelectorAll("[data-target]"),
+  tabContents = document.querySelectorAll("[data-content]");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("qualification_active");
+    });
+    target.classList.add("qualificaion_active");
+
+    tabs.forEach((tab) => {
+      tab.classList.remove("qualification_active");
+    });
+    tab.classList.add("qualification_active");
+  });
+});
 
 //  SERVICES MODAL
 

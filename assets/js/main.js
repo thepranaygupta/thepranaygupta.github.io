a58905bc2a04d535574a793c0c244bba4f042d2b
Pranay <pranayguptamain@gmail.com>
2021-12-20 20:29:21 +0530

    feat: Add skills section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,52 +1,62 @@
-const navMenu = document.getElementById('nav-menu'),
-    navToggle = document.getElementById('nav-toggle'),
-    navClose = document.getElementById('nav-close')
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
-//  MENU SHOW 
+//  MENU SHOW
 if (navToggle) {
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu')
-    })
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 }
 
-//  MENU HIDDEN 
+//  MENU HIDDEN
 if (navClose) {
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu')
-    })
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 
-//  REMOVE MENU MOBILE 
-const navLink = document.querySelectorAll('.nav_link')
+//  REMOVE MENU MOBILE
+const navLink = document.querySelectorAll(".nav_link");
 
 function linkAction() {
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show-menu')
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
-//  ACCORDION SKILLS
+// SKILLS
+const skillsContent = document.getElementsByClassName("skills_content"),
+  skillsHeader = document.querySelectorAll(".skills_header");
 
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills_content skills_close";
+  }
+
+  if (itemClass === "skills_content skills_close") {
+    this.parentNode.className = "skills_content skills_open";
+  }
+}
+
+skillsHeader.forEach((e) => {
+  e.addEventListener("click", toggleSkills);
+});
 
 //  QUALIFICATION TABS
 
-
 //  SERVICES MODAL
-
 
 //  PORTFOLIO SWIPER
 
-
 //  TESTIMONIAL
-
 
 //  SCROLL SECTIONS ACTIVE LINK
 
-
 //  CHANGE BACKGROUND HEADER
-
 
 //  SHOW SCROLL UP
 
-
-//  DARK LIGHT THEME  
+//  DARK LIGHT THEME

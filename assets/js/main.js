23042946d2063c5aa94562a7a1699f34f6561ade
Pranay <pranayguptamain@gmail.com>
2021-12-25 19:07:10 +0530

    feat: Highlight active links in navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -107,9 +107,29 @@ let swiper = new Swiper(".mySwiper", {
   keyboard: true,
 });
 
-//  TESTIMONIAL
-
 //  SCROLL SECTIONS ACTIVE LINK
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav_menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav_menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
 
 //  CHANGE BACKGROUND HEADER
 
